5ae01d3bd7853a49c2759e028620970210c4f79f
Isidro Arias <isidroariass@hotmail.es>
2024-08-10 18:24:12 +0200

    clang

--- math/prime_factoriziation_mine.c
+++ math/prime_factoriziation_mine.c
@@ -4,8 +4,8 @@
 unsigned int *prime_factors(unsigned int n)
 {
     unsigned *res = NULL, *current = NULL, i = 2;
-    res =  malloc(sizeof(unsigned) * 20);
-    unsigned size =0;
+    res = malloc(sizeof(unsigned) * 20);
+    unsigned size = 0;
     while (n > 1 && i <= n)
     {
         if (n % i)
@@ -16,12 +16,14 @@ unsigned int *prime_factors(unsigned int n)
         n = n / i;
         res[size] = i;
         size++;
-        if((size+1)%20==0){
-          // plus one because we have to leave room to the terminator
-          res = realloc(res, (size + 20)*sizeof(unsigned));
-          if (res == NULL) {
-            return NULL;
-          }
+        if ((size + 1) % 20 == 0)
+        {
+            // plus one because we have to leave room to the terminator
+            res = realloc(res, (size + 20) * sizeof(unsigned));
+            if (res == NULL)
+            {
+                return NULL;
+            }
         }
     }
     res[size] = 0;  // terminator
@@ -43,10 +45,10 @@ int main(void)
     assert(array[1] == 0);
     free(array);
 
-    array = prime_factors(1<< 31);
-    assert(array[0]==2);
-    assert(array[30]==2);
-    assert(array[31]==0);
+    array = prime_factors(1 << 31);
+    assert(array[0] == 2);
+    assert(array[30] == 2);
+    assert(array[31] == 0);
     free(array);
     return 0;
 }
